00b16fb7b86bb855da57dbe3e218eb605de422ba
Fritz-Michael <fritz_manalili@dlsu.edu.ph>
2017-06-17 19:51:11 +0800

    Hindi maayos yung layout nito paayos na lang kung natripan niyo

---
+++ Mp1.js
@@ -0,0 +1,19 @@
+var changeContents = function(header_id) {
+	var content;
+	if(header_id === "posts") {
+		content = "posts goes here";
+		document.getElementById("contents").textContent = content;
+	}
+	if(header_id === "profile") {
+		content = "profile goes here";
+		document.getElementById("contents").textContent = content;
+	}
+	if(header_id === "photos") {
+		content = "phtos goes here";
+		document.getElementById("contents").textContent = content;
+	}
+	if(header_id === "album") {
+		content = "album goes here";
+		document.getElementById("contents").textContent = content;
+	}
+}
