aab3ee74bc85433cea7bec56aacb7fed7cfc8130
KentRegs <matthewregalado90@yahoo.com>
2017-06-18 16:05:40 +0800

    Update Mp1.js

--- Mp1.js
+++ Mp1.js
@@ -1,20 +1,23 @@
-
 var changeContents = function(header_id) {
 	var content;
 	if(header_id === "posts") {
 		content = "posts goes here";
-		document.getElementById("contents").textContent = content;
+		document.getElementById("contents").textContent = content;		
+		document.getElementById('images').style.display = "none";
 	}
 	if(header_id === "profile") {
 		content = "profile goes here";
 		document.getElementById("contents").textContent = content;
+		document.getElementById('images').style.display = "none";
 	}
 	if(header_id === "photos") {
-		content = "phtos goes here";
+		content = "Sample Photos";
 		document.getElementById("contents").textContent = content;
+		document.getElementById('images').style.display = "block";
 	}
 	if(header_id === "album") {
 		content = "album goes here";
 		document.getElementById("contents").textContent = content;
+		document.getElementById('images').style.display = "none";
 	}
 }
