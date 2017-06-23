4af7b28ddd73faeb1b92775a56931aa2060e869d
Fritz-Michael <fritz_manalili@dlsu.edu.ph>
2017-06-23 16:41:42 +0800

    Used jQuery

--- Mp1.js
+++ Mp1.js
@@ -1,5 +1,5 @@
 
-var changeContents = function(header_id) {
+/*var changeContents = function(header_id) {
 	var content;
 	console.log(header_id);
 	if(header_id === "posts") {
@@ -18,4 +18,30 @@ var changeContents = function(header_id) {
 		content = "album goes here";
 		document.getElementById("contents").textContent = content;
 	}
-}
+}*/
+
+$(document).ready(function() {
+	$("#posts").click( function() {
+		var msg = "Posts goes here";
+
+		$("#contents").text(msg);
+	});
+
+	$("#profile").click( function() {
+		var msg = "Profile goes here";
+
+		$("#contents").text(msg);
+	});
+
+	$("#photos").click( function() {
+		var msg = "Photos goes here";
+
+		$("#contents").text(msg);
+	});
+
+	$("#album").click( function() {
+		var msg = "Album goes here";
+
+		$("#contents").text(msg);
+	});
+});
