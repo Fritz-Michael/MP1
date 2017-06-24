6896f25e26d3bbdf8014d41cb8ec17867d8e0afe
Fritz-Michael <fritz_manalili@dlsu.edu.ph>
2017-06-24 13:08:00 +0800

    Added ajax. Check the console to confirm that ajax is running

--- Mp1.js
+++ Mp1.js
@@ -25,23 +25,59 @@ $(document).ready(function() {
 		$("#mainDiv").empty();
 		$("#mainDiv").append("<div id = \"contentDiv\"><h1 id = \"contents\">Profile goes here</h1></div>");
 		$("#contentDiv").css("background", "blue");
+
+		//ajax part here
+		$.ajax({
+			type: 'GET',
+			url: 'https://jsonplaceholder.typicode.com/users',
+			success: function(data) {
+				console.log("sup world", data);
+			}
+		});
 	});
 
 	$("#profile").click( function() {
 		$("#mainDiv").empty();
 		$("#mainDiv").append("<div id = \"contentDiv\"><h1 id = \"contents\">Posts goes here</h1></div>");
 		$("#contentDiv").css("background", "yellow");
+
+		//ajax part here
+		$.ajax({
+			type: 'GET',
+			url: 'https://jsonplaceholder.typicode.com/posts',
+			success: function(data) {
+				console.log("sup world", data);
+			}
+		});
 	});
 
 	$("#photos").click( function() {
 		$("#mainDiv").empty();
 		$("#mainDiv").append("<div id = \"contentDiv\"><h1 id = \"contents\">Photos goes here</h1></div>");
 		$("#contentDiv").css("background", "green");
+
+		//ajax part here
+		$.ajax({
+			type: 'GET',
+			url: 'https://jsonplaceholder.typicode.com/photos',
+			success: function(data) {
+				console.log("sup world", data);
+			}
+		});
 	});
 
 	$("#album").click( function() {
 		$("#mainDiv").empty();
 		$("#mainDiv").append("<div id = \"contentDiv\"><h1 id = \"contents\">Album goes here</h1></div>");
 		$("#contentDiv").css("background", "red");
+
+		//ajax part here
+		$.ajax({
+			type: 'GET',
+			url: 'https://jsonplaceholder.typicode.com/albums',
+			success: function(data) {
+				console.log("sup world", data);
+			}
+		});
 	});
 });
