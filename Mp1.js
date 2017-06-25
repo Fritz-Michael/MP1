e6bfec7ae0b48113925b4d8ae20b91159beb252a
Fritz-Michael <fritz_manalili@dlsu.edu.ph>
2017-06-25 13:00:09 +0800

    Finished View posts and View a User

--- Mp1.js
+++ Mp1.js
@@ -1,58 +1,6 @@
 $(document).ready(function() {
 
-	// var globalId;
-
-	// var getUserId = function(username) {
-	// 	$.ajax({
-	// 		type: 'GET',
-	// 		url: 'https://jsonplaceholder.typicode.com/users',
-	// 		async: false,
-	// 		success: function(users) {
-	// 			$.each(users, function(i, users){
-	// 				if(users.username == username)
-	// 					globalId = users.id;
-	// 			});
-	// 		}
-	// 	});
-	// }
-
-	// $(document).on('click', '.userpost', function() {
-	// 	var elementId;
-
-	// 	elementId = $(this).attr("id");
-
-	// 	console.log(elementId);
-
-	// 	getUserId(elementId);
-
-	// 	console.log(globalId);
-	// 	$("#contentDiv").empty();
-	// 	$("#contentDiv").append("<p align = center>" + elementId + "'s posts </p>");
-	// 	$("#contentDiv").append("<button id = \"return\" align = center>Click here to return</button>");
-	// 	$.ajax({
-	// 		type: 'GET',
-	// 		url: 'https://jsonplaceholder.typicode.com/posts',
-	// 		success: function(posts) {
-	// 			$.each(posts, function(i,posts) {
-	// 				if(posts.userId == globalId) {
-	// 					console.log(posts.title);
-	// 					$("#contentDiv").append("<p align = \"center\"><b>" + posts.title + "</b></p>");
-	// 					$("#contentDiv").append("<p align = \"center\" class = \"textBody\">" + posts.title + "</p>");
-	// 					console.log("Im here");
-	// 				}
-	// 			});
-	// 			$("#contentDiv").append("<button id = \"return\" align = center>Show more</button>");
-	// 		},
-	// 		error: function() {
-	// 			alert("Oops! Something went wrong. Sorry about that.");
-	// 		}
-	// 	});
-	// });
-
 	$("#posts").click( function() {
-		var userId;
-		var users;
-		var username;
 
 		$("#mainDiv").empty();
 		$("#mainDiv").append("<div id = \"contentDiv\"></div>");
@@ -63,7 +11,7 @@ $(document).ready(function() {
 			url: 'https://jsonplaceholder.typicode.com/users',
 			success: function(users) {
 				$.each(users, function(i,users){
-					$("#contentDiv").append("<div class = \"userpost\" id = \"" + users.id + "\">" + users.username + "</div>");
+					$("#contentDiv").append("<div class = \"userpost\" id = \"" + users.id + "\"></div>");
 					$("#contentDiv").append("<br>");
 
 				});
@@ -82,9 +30,11 @@ $(document).ready(function() {
 					url: 'https://jsonplaceholder.typicode.com/users',
 					success: function(users) {
 						$.each(users, function(i,users) {
-							//$(".textTitle").eq(i).append("<p>hello</p>");
 							$(".textTitle.post" + users.id).append("<p class = \"textUser\"><a href = \"\">" + users.username + "</a></p>");
 						});
+					},
+					error: function() {
+						alert("Oops! Something went wrong. Please refresh the webpage");
 					}
 				});
 			},
@@ -94,42 +44,76 @@ $(document).ready(function() {
 		});
 	});
 
-	$(document).on("click", "#return", function() {
+	$("#profile").click( function() {
+
 		$("#mainDiv").empty();
 		$("#mainDiv").append("<div id = \"contentDiv\"></div>");
 		$("#contentDiv").css("background", "blue");
-
-		$.ajax({
-			type: 'GET',
-			url: 'https://jsonplaceholder.typicode.com/users',
-			success: function(users) {
-				$.each(users, function(i,users){
-					$("#contentDiv").append("<div class = \"userpost\" id = \"" + users.username + "\">" + users.username + "</button>");
-					$("#contentDiv").append("<br>");
-				});
-			},
-			error: function() {
-				alert("Oops! Something went wrong. Sorry about that.");
-			}
-		});
-	});
-
-
-	$("#profile").click( function() {
-		var count;
-
-		$("#mainDiv").empty();
-		$("#mainDiv").append("<div id = \"contentDiv\"></div>");
-		$("#contentDiv").css("background", "yellow");
 
 		//ajax part here
 		$.ajax({
 			type: 'GET',
 			url: 'https://jsonplaceholder.typicode.com/users',
-			success: function(data) {
-				$.each(data, function(i,data) {
-					$("#contentDiv").append("<br>");
-					$("#contentDiv").append(data[count].name);
+			success: function(users) {
+				$.each(users, function(i,users) {
+					$("#contentDiv").append("<div class = \"userProfile\" id = \"" + users.id + "\">" + users.username + "</div>");
+				});
+			},
+			error: function() {
+				alert("There is an error in loading. We are sorry about that");
+			}
+		});
+	});
+
+	$(document).on("click", ".userProfile", function(){
+		var userId;
+		console.log("hello");
+		$("#contentDiv").empty();
+		userId = $(this).attr("id");
+		
+		$.ajax({
+			type: 'GET',
+			url: 'https://jsonplaceholder.typicode.com/users',
+			success: function(users) {
+				$.each(users, function(i,users) {
+					if(userId == users.id) {
+						$("#contentDiv").append("<p align = center>" + users.name + "</p>");
+						$("#contentDiv").append("<p>Username: " + users.username + "</p>");
+						$("#contentDiv").append("<p>Email: " + users.email + "</p>");
+						$("#contentDiv").append("<p>Address:</p>");
+						$("#contentDiv").append("<p>Street: " + users.address.street + "</p>");
+						$("#contentDiv").append("<p>Suite: " + users.address.suite + "</p>");
+						$("#contentDiv").append("<p>City: " + users.address.city + "</p>");
+						$("#contentDiv").append("<p>Zip Code: " + users.address.zipcode + "</p>");
+						$("#contentDiv").append("<p>Phone Number: " + users.phone + "</p>");
+						$("#contentDiv").append("<p>Website: " + users.website + "</p>");
+						$("#contentDiv").append("<p>Company:</p>");
+						$("#contentDiv").append("<p>Name: " + users.company.name + "</p>");
+						$("#contentDiv").append("<p>Catch Phrase: " + users.company.catchPhrase + "</p>");
+						$("#contentDiv").append("<p>BS: " + users.company.bs + "</p>");
+					}
+				});
+				$("#contentDiv").append("<button id = \"returnProfiles\">Return to Profiles</button>");
+			},
+			error: function() {
+				alert("There is an error in loading. We are sorry about that");
+			}
+		});
+
+	});
+
+	$(document).on("click", "#returnProfiles", function() {
+		$("#mainDiv").empty();
+		$("#mainDiv").append("<div id = \"contentDiv\"></div>");
+		$("#contentDiv").css("background", "blue");
+
+		//ajax part here
+		$.ajax({
+			type: 'GET',
+			url: 'https://jsonplaceholder.typicode.com/users',
+			success: function(users) {
+				$.each(users, function(i,users) {
+					$("#contentDiv").append("<div class = \"userProfile\" id = \"" + users.id + "\">" + users.username + "</div>");
 				});
 			},
 			error: function() {
